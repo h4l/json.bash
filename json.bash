258e1f78d6f3700c2760723ac5197d84e36ba2d6
Hal Blackburn <hwtb2@cam.ac.uk>
2023-06-21 09:31:56 +0000

    perf: optimise encode_json_strings
    We now use a lookup table for escape sequences to avoid forking a
    subshell. We previously used printf to resolve escape codes, which
    required a subshell to capture its output. The hot_loop.sh example
    benchmark is ~2x faster with the custom encode_json_strings method now.

--- json.bash
+++ json.bash
@@ -3,19 +3,27 @@
 _json_bash_arg_pattern='^(@(\w+)|([^:=@-][^:=@]*))?(:(auto|bool|false|null|number|raw|string|true)(\[\])?)?(@=(\w+)|=(.*))?$'
 _json_bash_number_pattern='-?(0|[1-9][0-9]*)(\.[0-9]*)?([eE][+-]?[0-9]+)?'
 _json_bash_auto_pattern="\"(null|true|false|${_json_bash_number_pattern:?})\""
+declare -gA _json_bash_escapes=(
+                     [$'\x01']='\u0001' [$'\x02']='\u0002' [$'\x03']='\u0003'
+  [$'\x04']='\u0004' [$'\x05']='\u0005' [$'\x06']='\u0006' [$'\x07']='\u0007'
+  [$'\b']='\b'       [$'\t']='\t'       [$'\n']='\n'       [$'\x0b']='\u000b'
+  [$'\f']='\f'       [$'\r']='\r'       [$'\x0e']='\u000e' [$'\x0f']='\u000f'
+  [$'\x10']='\u0010' [$'\x11']='\u0011' [$'\x12']='\u0012' [$'\x13']='\u0013'
+  [$'\x14']='\u0014' [$'\x15']='\u0015' [$'\x16']='\u0016' [$'\x17']='\u0017'
+  [$'\x18']='\u0018' [$'\x19']='\u0019' [$'\x1a']='\u001a' [$'\x1b']='\u001b'
+  [$'\x1c']='\u001c' [$'\x1d']='\u001d' [$'\x1e']='\u001e' [$'\x1f']='\u001f')
 
 # Encode the positional arguments as JSON strings, joined by commas.
 function encode_json_strings() {
+  local strings joined literal escape
   strings=("${@//$'\\'/$'\\\\'}")             # escape \
   strings=("${strings[@]//$'"'/$'\\"'}")      # escape "
   strings=("${strings[@]/#/\"}")              # wrap in quotes
   strings=("${strings[@]/%/\"}")
   local IFS; IFS=${join:-,}; joined="${strings[*]}";  # join by ,
-  while [[ $joined =~ [[:cntrl:]] ]]; do      # Escape special chars if needed
-    declare -A escapes=([08]=b [09]=t [0a]=n [0c]=f [0d]=r)
-    literal=${BASH_REMATCH[0]}
-    hex=$(printf '%02x' "'$literal")
-    escape=$(printf '\\%s' "${escapes[$hex]:-u00$hex}")
+  while [[ $joined =~ [$'\x01'-$'\x1f\t\n\v\f\r'] ]]; do  # Escape special chars if needed
+    literal=${BASH_REMATCH[0]:?}
+    escape=${_json_bash_escapes[$literal]:?"no escape for ${literal@A}"}
     joined=${joined//$literal/$escape}
   done
   echo "$joined"
