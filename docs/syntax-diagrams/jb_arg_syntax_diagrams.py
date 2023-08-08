75f1d2f6599fbbdb5b5943106d34559140189cb7
Hal Blackburn <hwtb2@cam.ac.uk>
2023-08-08 12:21:55 +0000

    docs: document argument syntax with railroad diagrams
    Using https://tabatkins.github.io/railroad-diagrams/, really nice tool!

---
+++ docs/syntax-diagrams/jb_arg_syntax_diagrams.py
@@ -0,0 +1,220 @@
+import io
+from pathlib import Path
+
+from railroad import (
+    Terminal,
+    DiagramItem,
+    Diagram,
+    Choice,
+    Optional,
+    NonTerminal,
+    ZeroOrMore,
+    Group,
+    Sequence,
+    OneOrMore,
+    HorizontalChoice,
+)
+
+
+def ensure_diagram(diagram_item: DiagramItem) -> Diagram:
+    if isinstance(diagram_item, Diagram):
+        return diagram_item
+    return Diagram(diagram_item)
+
+
+def diagram(diagram_item: DiagramItem) -> Diagram:
+    if isinstance(diagram_item, Diagram):
+        raise ValueError("diagram_item must not be a Diagram")
+    # type="complex" actually makes the diagram simpler by using single | at the
+    # beginning and end e.g. |- ... -| rather than ||- ... --||
+    return Diagram(diagram_item, type="complex")
+
+
+# Key
+def flags() -> DiagramItem:
+    return OneOrMore(Choice(1, Terminal("+"), Terminal("~"), Terminal("?")))
+
+
+def key_escape() -> DiagramItem:
+    return HorizontalChoice(Terminal("::"), Terminal("=="), Terminal("@@"))
+
+
+def key_chars():
+    key_char = NonTerminal("Any char except :=@")
+    return OneOrMore(Choice(1, key_escape(), key_char))
+
+
+def key() -> DiagramItem:
+    start_of_bare_key = NonTerminal("Any char except .+~?:=@")
+    start_of_ref = Group(Terminal("@"), "reference")
+    start_of_key = Choice(1, start_of_ref, start_of_bare_key, Terminal("="))
+
+    return Sequence(
+        Optional(NonTerminal("flags")), start_of_key, Optional(NonTerminal("key-chars"))
+    )
+
+
+def ref_key() -> DiagramItem:
+    return Sequence(Terminal("@"), NonTerminal("key-chars"))
+
+
+def string_key() -> DiagramItem:
+    start_of_bare_key = NonTerminal("Any char except .+~?:=@")
+    start_of_str = Choice(0, start_of_bare_key, Terminal("="))
+    return Sequence(start_of_str, NonTerminal("key-chars"))
+
+
+def no_metadata_key() -> DiagramItem:
+    not_flag_or_start_char = NonTerminal("Any char except +~?:=@")
+    non_flag_key_char = Choice(1, key_escape(), not_flag_or_start_char)
+    return Sequence(NonTerminal("key"), non_flag_key_char)
+
+
+# Meta
+def type() -> DiagramItem:
+    return HorizontalChoice(
+        Terminal("string"),
+        Terminal("number"),
+        Terminal("bool"),
+        Terminal("true"),
+        Terminal("false"),
+        Terminal("null"),
+        Terminal("raw"),
+        Terminal("auto"),
+    )
+
+
+def object_collection() -> DiagramItem:
+    split_char = Group(NonTerminal("Any char"), "split")
+    object_format = Sequence(
+        Terminal(":"), Group(Choice(0, Terminal("attrs"), Terminal("json")), "format")
+    )
+    object_collection = Sequence(
+        "{", Optional(split_char), Optional(object_format), "}"
+    )
+    return object_collection
+
+
+def array_collection() -> DiagramItem:
+    split_char = Group(NonTerminal("Any char"), "split")
+    array_format = Sequence(
+        Terminal(":"), Group(Choice(0, Terminal("raw"), Terminal("json")), "format")
+    )
+    array_collection = Sequence("[", Optional(split_char), Optional(array_format), "]")
+    return array_collection
+
+
+def collection() -> DiagramItem:
+    return HorizontalChoice(
+        NonTerminal("array-collection"), NonTerminal("object-collection")
+    )
+
+
+def attribute_name() -> DiagramItem:
+    return OneOrMore(Choice(0, NonTerminal("Any char except /,="), "==", ",,", "//"))
+
+
+def attribute_value() -> DiagramItem:
+    return ZeroOrMore(Choice(0, NonTerminal("Any char except /,"), ",,", "//"))
+
+
+def attribute() -> DiagramItem:
+    return Sequence(
+        NonTerminal("attribute-name"),
+        Optional(Sequence("=", NonTerminal("attribute-value"))),
+    )
+
+
+def attributes() -> DiagramItem:
+    return Sequence("/", ZeroOrMore(NonTerminal("attribute"), repeat=","), "/")
+
+
+def metadata() -> DiagramItem:
+    return Sequence(
+        Terminal(":"),
+        Optional(NonTerminal("type")),
+        Optional(NonTerminal("collection")),
+        Optional(NonTerminal("attributes")),
+    )
+
+
+def value() -> DiagramItem:
+    value_start = Choice(1, Group("@", "ref"), "=")
+    return Sequence(Optional(NonTerminal("flags")), value_start, "Any char")
+
+
+def argument() -> DiagramItem:
+    key_and_meta = Choice(
+        0,
+        Sequence(NonTerminal("key"), NonTerminal("metadata")),
+        NonTerminal("no-metadata-key"),
+    )
+    return Sequence(
+        Optional(Group("...", "splat")),
+        Optional(key_and_meta),
+        Optional(NonTerminal("value")),
+    )
+
+
+def render_diagram(diagram: Diagram, *, standalone: bool = False) -> str:
+    out = io.StringIO()
+    if standalone:
+        diagram.writeStandalone(out.write)
+    else:
+        diagram.writeSvg(write=out.write)
+    return out.getvalue()
+
+
+def render_html(diagrams: dict[str, Diagram]) -> str:
+    sections = [
+        f"<h2>{name}</h2>\n{render_diagram(diagram)}"
+        for (name, diagram) in diagrams.items()
+    ]
+    nl = "\n"
+    return f"""\
+<!doctype html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <title>Diagram</title>
+  <link rel="stylesheet" href="diagram.css">
+</head>
+
+<body>
+<h1>Diagrams</h1>
+{nl.join(sections)}
+</body>
+
+</html>
+
+"""
+
+
+def main():
+    diagrams = {
+        "flags": diagram(flags()),
+        "key-chars": diagram(key_chars()),
+        "key": diagram(key()),
+        "no-metadata-key": diagram(no_metadata_key()),
+        "object-collection": diagram(object_collection()),
+        "array-collection": diagram(array_collection()),
+        "collection": diagram(collection()),
+        "attribute-name": diagram(attribute_name()),
+        "attribute-value": diagram(attribute_value()),
+        "attribute": diagram(attribute()),
+        "attributes": diagram(attributes()),
+        "metadata": diagram(metadata()),
+        "type": diagram(type()),
+        "value": diagram(value()),
+        "argument": diagram(argument()),
+    }
+    diagram_html = render_html(diagrams)
+    Path("diagram.html").write_text(diagram_html)
+
+    for name, diag in diagrams.items():
+        Path(f"{name}.svg").write_text(render_diagram(diag, standalone=True))
+
+
+if __name__ == "__main__":
+    main()
---
+++ docs/syntax.md
@@ -0,0 +1,166 @@
+# Argument Syntax
+
+`json.bash`'s arguments define an entries in a JSON object or array. Arguments
+use the syntax described here to define the key and value of their entries, and
+metadata on their JSON type (number, bool, etc) and how to handle missing/empty
+files, etc.
+
+Arguments have 3 main parts — the [`key`](#key), [`metadata`](#metadata) and
+[`value`](#value), which are all optional.
+
+![argument diagram](syntax-diagrams/argument.svg)
+
+The optional `...` splat prefix makes the argument describe multiple entries
+that are merged into the host object or array.
+
+## `key`
+
+Keys can be prefixed with [Flags](#flags) to control their behaviour when
+missing/empty.
+
+Keys starting with `@` are references to variables or files. (File references
+start with `/` or `./`, otherwise they're variables). Otherwise, the key's value
+is a literal string. Values that use flag characters need to start with an `=`.
+
+![argument diagram](syntax-diagrams/key.svg)
+
+### `key-chars`
+
+The main value of a key can be any character, except `:` which starts a
+[`meta`](#meta) section, and `@` or `=` which start a [`value`](#value). To
+include these characters in keys, they must be escaped by doubling them.
+
+![argument diagram](syntax-diagrams/key-chars.svg)
+
+### `no-metadata-key`
+
+Keys that are not followed by [`meta`](#meta) can't end with the flag
+characters, as they could be confused with the flags for the argument's
+[`value`](#value).
+
+![argument diagram](syntax-diagrams/no-metadata-key.svg)
+
+## `flags`
+
+Flags can appear before keys and values to control what happens when an entry's
+key or value is missing or empty.
+
+- `+` restores strictness, causing an error if the item is missing or empty (the
+  `json_defaults` option can make the default non-strict).
+- `~` causes an un-set variable or missing file to be treated as if it was
+  empty.
+- `?` causes empty items to use a default value, according to the item's JSON
+  type and the item's source (var, file, str).
+- `??` causes entries containing the empty item to be omitted from the host
+  object / array.
+
+![argument diagram](syntax-diagrams/flags.svg)
+
+## `metadata`
+
+Arguments have metadata to control how their values are represented in their
+entry of the produced JSON object or array. There are three parts to the
+metadata, all optional:
+
+The [`type`](#type)
+
+![argument diagram](syntax-diagrams/metadata.svg)
+
+### `type`
+
+An argument's type defines the the JSON type of the entry's value. The default
+type is `string`, so unless the default is changed using `$json_default`, a type
+must be specified to create numbers, bools, etc.
+
+The `string` type encodes the input as a JSON string, so any value is valid. The
+`null`, `true` and `false` types don't require a redundant value. The `auto`
+type produces numbers, bool and null values when values match those types, and
+strings otherwise. The `json` type validates that the input is valid JSON and
+emits it unchanged. The `raw` acts like `json`, except that it performs no
+validation!
+
+![argument diagram](syntax-diagrams/type.svg)
+
+### `collection`
+
+An argument marked with a collection creates an entry with a variable-length
+array or object as its value, rather than a single value.
+
+The collection section can include a split char and format. The split char
+defines which character is used to split the input file, variable, or string
+into chunks. Each chunk of the input is converted to array or object entries
+according to the _format_.
+
+![argument diagram](syntax-diagrams/collection.svg)
+
+#### `array-collection`
+
+The default format is `raw`.
+
+- The `raw` format validates each chunk against the argument's [`type`](#type)
+- The `json` format validates that chunks are JSON arrays containing the
+  argument's [`type`](#type). The entries of each chunk's array are concatenated
+  to form a single JSON array.
+
+![argument diagram](syntax-diagrams/array-collection.svg)
+
+#### `object-collection`
+
+The default format is `attrs`.
+
+- The `attrs` format reads each chunk of input using the same `name=value`
+  format as is used for [`attributes`](#attributes) (except that `/` is not
+  reserved or escaped). All `name=value` pairs are merged to form entries in a
+  single JSON object.
+- The `json` format validates that chunks are JSON objects with values that
+  match the argument's [`type`](#type). The entries of each chunk's object are
+  concatenated to form a single JSON object.
+
+![argument diagram](syntax-diagrams/object-collection.svg)
+
+### `attributes`
+
+Attributes are a series of name-value pairs inside a pair of slashes, such as
+`/a=b,c=d/`.
+
+Like the values of keys, attributes can double up characters to escape them.
+
+![argument diagram](syntax-diagrams/attributes.svg)
+
+#### `attribute`
+
+An individual attribute can just be a name without a value.
+
+![argument diagram](syntax-diagrams/attribute.svg)
+
+#### `attribute-name`
+
+Names must escape `=`, `,` and `/` characters.
+
+![argument diagram](syntax-diagrams/attribute-name.svg)
+
+Values must escape `,` and `/` characters, but don't need to escape `=`.
+
+#### `attribute-value`
+
+![argument diagram](syntax-diagrams/attribute-value.svg)
+
+## `value`
+
+Values can be prefixed with [Flags](#flags) to control their behaviour when
+missing/empty.
+
+Values starting with `@` are references to variables or files. (File references
+start with `/` or `./`, otherwise they're variables). Otherwise, a value starts
+with `=` and the remaining text is the argument's value, without any escaping or
+further parsing.
+
+![argument diagram](syntax-diagrams/value.svg)
+
+> ### Warning ‼️
+>
+> To be parsed unambiguously, a value needs to be preceded by either a key or
+> type marker, for example `foo=bar` or `:=bar`. An argument with `=` at the
+> start such as `=bar` defines a [`key`](#key), which is subject to double-char
+> escaping and further parsing to identify [`metadata`](#metadata) and
+> [`value`](#value) sections.
